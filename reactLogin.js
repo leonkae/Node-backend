e2c42d33e8ff471a543aed2b563a0303e47decb6
leonkae <leon29kae@gmail.com>
2025-06-19 20:23:14 +0300

    Simple example of login being handled in react

---
+++ reactLogin.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:5000/login', {
+        username, password
+      }, { withCredentials: true });
+
+      if (res.data.success) {
+        navigate('/home');
+      }
+    } catch (err) {
+      alert('Login failed!');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <input value={username} onChange={e => setUsername(e.target.value)} placeholder="Username" />
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
